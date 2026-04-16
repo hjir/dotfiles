1fbf10fdb16d5e4c12bd8ba29bde70ba06704805
Hana Jirovska <h.jirovska@tudelft.nl>
2026-04-16 14:28:55 +0200

    Fix order

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
+echo "==> Copying custom themes..."
+cp -r "$DOTFILES_DIR/custom/themes/." "$HOME/.oh-my-zsh/custom/themes/"
+
 echo "==> Linking oh-my-zsh..."
 ln -sf "$DOTFILES_DIR/.oh-my-zsh" "$HOME/.oh-my-zsh"
 
 echo "==> Linking .zshrc..."
 ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
-
-echo "==> Copying custom themes..."
-cp -r "$DOTFILES_DIR/custom/themes/." "$HOME/.oh-my-zsh/custom/themes/"
 
 echo "==> Setting zsh as default shell..."
 sudo chsh -s "$(which zsh)" "$(whoami)"
